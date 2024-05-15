1ff4fbacbd244f36f3ba5c471df78c982e2c558e
Facundo Moreno <1913100113@untels.edu.pe>
2024-05-15 11:03:48 -0500

    Proyecto terminado

--- app/src/main/java/com/example/dam_facundomoreno/MainActivity.java
+++ app/src/main/java/com/example/dam_facundomoreno/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.dam_facundomoreno;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn1 = findViewById(R.id.button2);
+        btn1.setOnClickListener(view -> Toast.makeText(this, "Clic en el Botón Enviar", Toast.LENGTH_SHORT).show());
+
+        TextView tv2 = findViewById(R.id.textViewg);
+        tv2.setOnClickListener(view -> Toast.makeText(this, "Se envió sus datos", Toast.LENGTH_SHORT).show());
     }
 }
